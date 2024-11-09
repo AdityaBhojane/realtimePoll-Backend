e1488905ece98b968198df89cb6f0b481db473de
aditya bhojane <bhojaneaditya7@gmail.com>
2024-11-09 20:13:47 +0530

    add cors code

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const { Server } = require('socket.io');
+
 
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
